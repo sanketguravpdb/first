5f08a19fce4adde3c489bfb429ec20778f392a93
sanketguravpdb <sanket.gurav@pdbtechsolutions.com>
2022-03-02 15:43:31 +0530

    your commit message here

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
        
-          Learn React
+          sanket gurav
         
       </header>
     </div>
